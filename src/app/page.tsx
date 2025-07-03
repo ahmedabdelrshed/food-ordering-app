8a254006860d754b1f331e0feb1d0f6ff4300fb6
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2025-07-03 18:42:19 +0300

    Add main colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 
 export default function Home() {
   return (
-    <main></main>
+    <main>
+      <h1 className="text-primary hover:text-primary ">
+        Welcome to the Food Ordering App
+      </h1>
+      <p>Order your favorite food online!</p>
+
+      {/* Add more components or content here */}
+    </main>
   );
 }
